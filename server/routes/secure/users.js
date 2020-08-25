cd160fc21f37f44c802d536437aa9595ac1adfb2
talialina <natalia@nataliamolina.com>
2020-08-25 19:40:54 -0400

    added welcome and sorry to see you go email

--- server/routes/secure/users.js
+++ server/routes/secure/users.js
@@ -72,5 +72,6 @@ router.delete('/api/users/me', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: e.toString() });
   }
+  console.log(hi);
 });
 module.exports = router;
