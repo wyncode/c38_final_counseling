93c143332a80a37e3e8912d12ea869ef5b2d5f0a
Jorge Silva <Jorgesilva1892@gmail.com>
2020-08-30 17:08:15 -0400

    journalEntry and Calendar componets. submiting now to seek asstience with rendering the calendar

---
+++ client/src/components/Calendar.jsx
@@ -0,0 +1,16 @@
+/*import React, { useState } from 'react';
+import Calendar from 'react-calendar';*/
+
+// unable to get calendar to rinder pushing to get some help while i debug.
+/*const Calender = () => {
+  const [date, setDate] = useState(new Date());
+  const onChange = (date) => {
+    setDate(date);
+  };
+};
+return (
+  <div>
+    <Calendar onChange={onChange} value={date} />
+  </div>
+);
+export default Calender; */
---
+++ client/src/components/JournalEntry.jsx
@@ -0,0 +1,36 @@
+/*import React from 'react';
+import { Container, Form, Button } from 'react-bootstrap';*/
+/*
+journal form component commented out till I can make a connection from the calendar to the jouranl entery
+function JournalEntry() {
+  return (
+    <Container>
+      <Form>
+        <Form.Group controlId="exampleForm.ControlInput1">
+          <Form.Label>Journal Title</Form.Label>
+          <Form.Control type="text" placeholder="Journal Title" />
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlSelect1">
+          <Form.Label>Mood</Form.Label>
+          <Form.Control as="select">
+            <option>Happy</option>
+            <option>Sad</option>
+            <option>Grumpy</option>
+            <option>Motivated</option>
+            <option>Excited</option>
+            <option>Bored</option>
+            <option>Indiferent</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>How do you feel like today?</Form.Label>
+          <Form.Control as="textarea" rows="3" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
+}
+export default JournalEntry;*/
