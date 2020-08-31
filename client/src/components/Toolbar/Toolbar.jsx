62966e9eb3edbfd67253e58c294ee3ae7f5f4b4e
amgarz0n <angigarzon@gmail.com>
2020-08-31 13:25:45 -0400

    left the hamburger menu instead of links, deleted css that will not be used

--- client/src/components/Toolbar/Toolbar.jsx
+++ client/src/components/Toolbar/Toolbar.jsx
@@ -1,47 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
+// import Nav from 'react-bootstrap/Nav';
 import Logo from './mind_passage_logo-white.png'
-// import DrawerToggleButton from '../SideDrawer/DrawerToggleButton'
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton'
 import "./Toolbar.css"
 
 const Toolbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
+    let handleToggle = () => {
+        setMenuOpen(true);
+    }
+
     return (
         <header className="header">
             <nav className="navigation">
-                {/* <div>
-                    <DrawerToggleButton />
-                </div> */}
+
                 <div className="nav-logo-icon">
                     <Link to="/home"><img src={Logo} alt="Mind Passage Home" /></Link>
                 </div>
-
-                <div className="spacer" />
-
-                <div className="nav-items">
-                    <Nav defaultActiveKey="/" as="ul">
-                        <Nav.Item as="li">
-                            <Link to="/home">Home</Link>
-                        </Nav.Item>
-
-                        <Nav.Item as="li">
-                            <Link to="/therapist-search">Find a Therapist</Link>
-                        </Nav.Item>
-
-                        <Nav.Item as="li">
-                            <Link to="/mental-hygiene">Mental Hygiene</Link>
-                        </Nav.Item>
-
-                        <Nav.Item as="li">
-                            <Link to="/profile">Your Profile</Link>
-                        </Nav.Item>
-
-                        <Nav.Item as="li">
-                            <Link to="/about">About Us</Link>
-                        </Nav.Item>
-
-                    </Nav>
+                <div>
+                    <DrawerToggleButton onClick={handleToggle} />
                 </div>
+
             </nav>
         </header>
 
