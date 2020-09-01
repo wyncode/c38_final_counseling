36858115a2cd6dc031ed574cea9208d2de48d74f
amgarz0n <angigarzon@gmail.com>
2020-09-01 19:57:43 -0400

    Added the Unhealthy Thinking content

--- client/src/pages/UnhealthyThinking/UnhealthyThinking.jsx
+++ client/src/pages/UnhealthyThinking/UnhealthyThinking.jsx
@@ -1,16 +1,72 @@
 import React from 'react';
+import ScrollDown from './down-06.png';
+import './UnhealthyThinking.css'
 
 const UnhealthyThinking = () => {
-  //this page is not displaying. Will come back to update it. 
-  return (
-    <main>
-      <div className="article-image">
-        <h1>Unhealthy Thinking</h1>
+        return (
+                <main className="article">
+                        <div className="article-header">
+                                <div className="article-title-author">
+                                        <h2>Unhealthy Thinking</h2>
+                                        <h1>10 Common Unhealthy Thinking Patterns (and How to Change Them)</h1>
+                                        <h3>By Caitlin Taylor</h3>
+                                        <h4>March 16, 2017 at 11:24pm</h4>
+                                        <img className="scroll-down" src={ScrollDown} alt="scroll down" />
+                                </div>
 
-      </div>
-    </main>
+                                <div className="article-image" />
+                        </div>
 
-  )
+
+                        <div className="article-copy">
+                                <p>Do the voices in your head ever make you feel like you’ve completely and irrevocably lost your mind? Are you thinking yourself in circles? Is it sometimes difficult to concentrate? You’re not alone. In fact, experts have pegged common patterns to some of our most unhealthy thoughts. Even better news? If you learn how to recognize these negative thinking patterns, it becomes easier to change them. Here’s 10 of the most common unhealthy thinking patterns and some suggestions on how to curb the negative spirals.</p>
+                                <p>Note: Anxiety, depression and other mental health disorders may heighten unhealthy thought patterns and may not be able to be maintained without the help of a mental health professional. If you, or someone you know, may be facing a mental health concern, Her Campus at MSU encourages you to seek out resources or call the national mental health hotline at <span>1-800-950-6264</span>.</p>
+                                <h2>1. All-or-Nothing Thinking</h2>
+                                <p>This is the failure to recognize grey areas or compromises — often paired with words like “never” or “always.” It’s thinking in absolute terms. I will never find someone who loves me. My mornings are always terrible. If I can’t have you as my partner, I don’t want you in my life at all. Life doesn’t occur in black-and-white -- always in shades of grey. When experiencing this type of thinking, it is important to remind yourself that what you are experiencing probably isn’t one extreme or another, but somewhere in between. Center yourself.</p>
+                                <h2>2. Blaming</h2>
+                                <p>This is our inability to recognize our own roles or capabilities in a situation. It’s often paired with using words like “helpless” or “hopeless” to describe ourselves.
+                                It’s not my fault that I was late -- I’m just really busy. Trying to meet all of my deadlines is hopeless. I might not have called, but they could have called me too.
+Sometimes we can be our own worst enemy. If it feels like things just keep going wrong, think about your role in the situations. Are you really just having bad luck, or are you not giving it your all? Hold yourself accountable.</p>
+                                <h2>3. Catastrophizing</h2>
+                                <p>This is when we talk about situations as worse than they actually are -- often to avoid truly dealing with them. A major sign of catastrophizing is failing to see positive outcomes as legitimate possibilities.
+                                This is the worst thing that has ever happened to me. There’s no way that this could ever get any better. I can’t look on the bright side -- there is no bright side.
+You’re not Chicken Little, and the sky isn’t really falling -- even if it feels like it is. When experiencing this type of thinking, remind yourself of the great things in your life to drown out negative thoughts. Stay grounded.</p>
+                                <h2>4. Double Standard</h2>
+                                <p>This is when we hold ourselves to a different -- often higher -- standard than we would apply to a friend. It’s when we are exceptionally hard on ourselves, without cutting ourselves some slack.
+                                If you would have just studied more, you would have gotten a better grade. It’s OK for my best friend to be single, but I should be in a relationship by now. You’ll never get a job if you get that tattoo.
+Reach out to a friend for this one. When experiencing this type of thinking, your friend will point out the double standard and remind you that you’re doing a good job. Give yourself some credit.</p>
+                                <h2>5. False Permanence</h2>
+                                <p>This is when we think that a negative situation or feeling is going to last forever, without the possibility of improvement. Sometimes, it happens when we don’t forgive ourselves for past mistakes.
+                                I’ll never live this down. My relationship won’t ever get better. Is this what the rest of my life is going to be like?
+Remember in high school when you thought you would never get past that embarrassing moment with your crush? You got past it. When experiencing this type of thinking, reflect back on past experiences to remind yourself of how you’ve made it through negative situations before. This too shall pass.</p>
+                                <h2>6. Magical Thinking</h2>
+                                <p>This is the belief that life would be better, if only we were thinner, prettier, smarter, wealthier or other damaging expectations. It can also be when we assume responsibility for things that are out of our control.
+                                If I were in better shape, my partner wouldn’t have cheated. I would be much happier if I made more money. If I would have known that interview question, I would have gotten the job.
+Cue in the patriarchy. When experiencing this type of thinking, remind yourself that women are held to an impossibly high standard in American society, and we can’t predict the future. You are perfect as you are -- remind yourself of that. Never change.</p>
+                                <h2>7. Mental Filter</h2>
+                                <p>This is when we focus only on the negative aspects of a situation, while completely ignoring the positive. It’s minimizing the good, in place of the bad.
+                                It doesn’t matter that I got a good score, the professor is an easy grader. The day was ruined because we got into an argument. The party was terrible because it rained.
+We’re our own biggest critic, but we need to become our own biggest cheerleader. When experiencing this type of thinking, remind yourself that it’s OK to celebrate the good instead of the bad. You’ve earned it.</p>
+                                <h2>8. Mind Reading</h2>
+                                <p>This is when we assume what other people are thinking or feeling, without really knowing at all. It’s jumping to conclusions with little or no evidence.
+                                I can just tell they aren’t having any fun. They probably don’t want to come to the party because of me. I know they don’t love me as much as I love them.
+In the world of social media, it can be hard not to draw your own conclusions. When experiencing this type of thinking, take a step back and ask someone what they’re truly thinking or feeling. Remember what “they” say about people who assume…</p>
+                                <h2>9. Overgeneralization</h2>
+                                <p>This is when a single event becomes falsely associated with a larger pattern. It’s when we make generalizations about isolated cases.
+                                Since they broke up with me, I must not be marriage material. I’ll never get into grad school after failing that test. If they lied to me once, they must lie to me all the time.
+We’ve all done it. When experiencing this type of thinking, remind yourself to focus on a single issue at a time. Isolate your thoughts in an effort to prevent yourself from assigning false patterns to past experiences. Take a deep breath.</p>
+                                <h2>10. Should Statements</h2>
+                                <p>This is setting standards that criticize ourselves, others or the world. It’s focusing on how things “should” be, rather than how they really are.
+                                I should have seen that coming. I should be over this by now. They should have known that I was having a bad day.
+To borrow the words of Carrie Bradshaw, we have to let go of the shoulda, coulda, wouldas, buckle up, and keep on going. When experiencing this type of thinking, remind yourself to live in the moment and attempt to stop holding yourself (and others) to unrealistic standards. Be where your feet are.</p>
+                        </div>
+
+                        <div className="article-source">
+                                <a href="https://www.hercampus.com/school/msu/10-common-unhealthy-thinking-patterns-and-how-change-them">The original article can be found here</a>
+                        </div>
+                </main >
+
+        )
 }
 
 export default UnhealthyThinking;
