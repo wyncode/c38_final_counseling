55df71c04b73e8a612b342b10e0c0957c1c992ce
Jorge Silva <Jorgesilva1892@gmail.com>
2020-09-02 10:59:16 -0400

    adding the journal list component

--- client/src/components/JournalList.jsx
+++ client/src/components/JournalList.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useContext } from 'react';
 import { Container, Table } from 'react-bootstrap';
 import axios from 'axios';
 import { AppContext } from '../context/AppContext';
-import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
 const JournalList = () => {
   const { setJournalList } = useContext(AppContext);
 
@@ -20,6 +21,33 @@ const JournalList = () => {
       });
   }, [setJournalList]);
 
-  return <Container></Container>;
+  return (
+    <Container>
+      <Accordion defaultActiveKey="0">
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="0">
+            View Journal Entrys!
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>Today was a good day</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    Happy
+                  </Card.Subtitle>
+                  <Card.Text>
+                    I had an amazing interview with a great company today.
+                  </Card.Text>
+                  <Card.Link href="#">Edit Journal</Card.Link>
+                  <Card.Link href="#">Delete Journal</Card.Link>
+                </Card.Body>
+              </Card>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
+    </Container>
+  );
 };
 export default JournalList;
