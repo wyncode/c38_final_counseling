387470f1d10ce92ba5665c189f2c4dd31b61dc41
dianaccanelon <dianaccanelon@gmail.com>
2020-09-01 23:05:50 -0400

    therapist search populating cards, changed therapist model and seed file

--- server/routes/secure/therapists.js
+++ server/routes/secure/therapists.js
@@ -2,9 +2,8 @@ const router = require('express').Router(),
   mongoose = require('mongoose'),
   Therapist = require('../../db/models/Therapist');
 // gets the therapist from our search bar needs to be tested after we populate our data base with faker
-
 //create a therapist
-router.post('/api/therapist/:id', async (req, res) => {
+router.post('/api/therapists/:id', async (req, res) => {
   const {
     name,
     jobTitle,
@@ -44,14 +43,41 @@ router.post('/api/therapist/:id', async (req, res) => {
     res.status(400).json({ error: error.toString() });
   }
 });
-
 //therapist search
+router.post('/api/therapists', async (req, res) => {
+  //sample postman request
+  //http://localhost:8080/api/therapists?price=100&location=Miami&race=black
+  const {
+    name,
+    jobTitle,
+    gender,
+    streetAddress,
+    zipCode,
+    city,
+    race,
+    specialty,
+    treatmentOrientation,
+    language,
+    cost
+  } = req.query;
 
-router.get('/api/Therapist', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const searchObject = {};
+
+  console.log(1, searchObject);
+
+  updates.forEach((update) => {
+    if (!req.body[update]) return null;
+    searchObject[update] = req.body[update];
+  });
+  console.log(2, searchObject);
   try {
-    const therapist = req.therapist;
-    res.json(therapist);
+    //const therapist = req.therapist;
+    const therapists = await Therapist.find(searchObject);
+    res.json(therapists);
   } catch (error) {
-    console.log('No availble Therapist, please try a different search.');
+    res.status(400).json({ error: error.toString() });
   }
 });
+
+module.exports = router;
