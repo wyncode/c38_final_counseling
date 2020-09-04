9e1a0087965a3bd9528985be6b9c9a1baba315b8
amgarz0n <angigarzon@gmail.com>
2020-09-03 20:08:07 -0400

    switched pngs for svgs, also added some media queries in UnhealthyThinking.css, Toolbar.css and DrawerToggleButton.css

--- client/src/components/Toolbar/Toolbar.jsx
+++ client/src/components/Toolbar/Toolbar.jsx
@@ -1,44 +1,31 @@
-import React from 'react';
+import React from 'react'
 import { Link, useHistory } from 'react-router-dom';
-import Logo from './mind_passage_logo-white.png';
-import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
-import BackButton from './back-button.png';
-import { useLocation } from 'react-router-dom';
-
-import './Toolbar.css';
+import Logo from './mind_passage_logo.svg'
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton'
+import BackButton from './back-button.svg'
+import "./Toolbar.css"
 
 const Toolbar = ({ setOpen, open }) => {
-  const history = useHistory(),
-    location = useLocation();
-
-  console.log(location);
+  const history = useHistory();
 
   return (
     <header className="header">
       <nav className="navigation">
-        <div className="">
-          {!location.pathname === '/' && (
-            <img
-              className="back-button"
-              src={BackButton}
-              alt="Go Back"
-              onClick={() => history.goBack()}
-            />
-          )}
-        </div>
+
+        <div className=""><img className="back-button" src={BackButton} alt="Go Back" onClick={() => history.goBack()} /></div>
 
         <div className="nav-logo-icon">
-          <Link to="/home">
-            <img src={Logo} alt="Mind Passage Home" />
-          </Link>
+          <Link to="/home"><img src={Logo} alt="Mind Passage Home" /></Link>
         </div>
 
         <div>
           <DrawerToggleButton setOpen={setOpen} open={open} />
         </div>
+
       </nav>
     </header>
-  );
+
+  )
 };
 
 export default Toolbar;
